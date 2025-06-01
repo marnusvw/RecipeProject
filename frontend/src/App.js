5a2b9a508c0a1aaf8e4b40550fd718f70c2e4f9b
Marnus von Wielligh <52937047+marnusvw@users.noreply.github.com>
2025-06-01 16:18:47 +0200

    Added a get to fetch all recipes from the db.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,15 @@ import {
 } from "react-router-dom";
 import Users from "./components/users";
 import HomePage from "./components/homePage";
+import Recipes from "./components/recipes";
+import RecipesPage from "./components/RecipesPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<HomePage />}>
       <Route path="users" element={<Users />} />
+      <Route path="recipes" element={<Recipes />} />
+      <Route path="recipes/:title" element={<RecipesPage />} />
     </Route>
   )
 );
--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,19 @@
 import { API_ENDPOINT } from ".";
 
+// Users API:
 export const getUsers = async () => {
-  const response = await fetch(`${API_ENDPOINT}/users`);
+  const response = await fetch(`${API_ENDPOINT}/users`, {
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+  const jsonResponse = await response.json();
+  return jsonResponse;
+};
+
+// Recipes API:
+export const getAllRecipes = async () => {
+  const response = await fetch(`${API_ENDPOINT}/recipes`);
   const jsonResponse = await response.json();
 
   return jsonResponse;

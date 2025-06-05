8808565863e6ecd563a9bf492337f46e98761256
Marnus von Wielligh <52937047+marnusvw@users.noreply.github.com>
2025-06-05 13:52:45 +0200

    Added the routeLoader to the laoders index page.

--- server/loaders/index.js
+++ server/loaders/index.js
@@ -1,12 +1,15 @@
 const expressLoader = require("./express");
 const passportLoader = require("./passport");
-
+const routeLoader = require("../routes");
 module.exports = async (app) => {
   // Load express middleware:
   const expressApp = await expressLoader(app);
 
   // Load passport middleware:
   const passport = await passportLoader(expressApp);
+
+  // Load API route handler:
+  await routeLoader(app, passport);
 
   app.use((err, req, res, next) => {
     const { message, status } = err;

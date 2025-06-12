cfeb04773e3a8b69658001656fcc4d9a549332aa
Marnus von Wielligh <52937047+marnusvw@users.noreply.github.com>
2025-06-12 15:44:16 +0200

    Add a basic Recipe Page to test single recipe retrieval.

--- frontend/src/components/RecipesPage.js
+++ frontend/src/components/RecipesPage.js
@@ -1,8 +1,37 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getRecipeByName } from "../api/api";
+import styles from "./styles/recipesPage.module.css";
 
 function RecipesPage() {
-  const { title } = useParams();
-  return <div>{title}</div>;
+  const { recipe_name } = useParams();
+  const [recipe, setRecipe] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const findRecipe = async () => {
+      try {
+        const recipe = await getRecipeByName(recipe_name);
+        console.log("Recipe: ", recipe);
+        setRecipe(recipe);
+      } catch (err) {
+        throw new Error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    findRecipe();
+  }, [recipe_name]);
+  if (loading) {
+    return <div className={styles.spinner}></div>;
+  }
+  return (
+    <ul>
+      {recipe.Ingredients?.map((ing) => (
+        <li key={ing.id}>{ing.ingredient}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default RecipesPage;

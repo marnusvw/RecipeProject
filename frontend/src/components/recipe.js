67e1d90e714ba0e953891bb7b0664e75b5fa4236
Marnus von Wielligh <52937047+marnusvw@users.noreply.github.com>
2025-06-12 15:43:40 +0200

    Create a basic layout for the recipes.

--- frontend/src/components/recipe.js
+++ frontend/src/components/recipe.js
@@ -3,10 +3,14 @@ import styles from "./styles/recipes.module.css";
 
 function Recipe({ recipe }) {
   return (
-    <Link to={`/recipes/${recipe.title}`}>
+    <Link to={`/recipes/${recipe.recipe_name}`}>
       <div className={styles.card}>
-        <h1 className={styles.h1}> {recipe.title}</h1>
-        <img className={styles.img} src={recipe.image_url} alt={recipe.title} />
+        <h1 className={styles.h1}> {recipe.recipe_name}</h1>
+        <img
+          className={styles.img}
+          src={recipe.recipe_image}
+          alt={recipe.recipe_name}
+        />
       </div>
     </Link>
   );

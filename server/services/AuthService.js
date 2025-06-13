657d8e5d365f815bed712cdb78fa74badd70cfe6
Marnus von Wielligh <52937047+marnusvw@users.noreply.github.com>
2025-06-13 12:40:57 +0200

    Update auth service to return success boolean to determine if registration was a success.

--- server/services/AuthService.js
+++ server/services/AuthService.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcrypt");
 module.exports = class AuthService {
   async register(data) {
     const { email } = data;
-    const SALT_ROUNDS = 12;
+    const SALT_ROUNDS = 5;
 
     try {
       const existingUser = await User.findOne({ where: { email } });
@@ -19,7 +19,8 @@ module.exports = class AuthService {
       const hashedPassword = await bcrypt.hash(data.password, SALT_ROUNDS);
       data.password = hashedPassword;
       const newUser = await User.create(data);
-      return newUser;
+      console.log("New User: ", newUser);
+      return { success: true, user: newUser };
     } catch (err) {
       throw createError(500, err.message || err);
     }

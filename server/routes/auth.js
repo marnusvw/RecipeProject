94df1f437a93b50eadd48e82d2f1cca27753b355
Marnus von Wielligh <52937047+marnusvw@users.noreply.github.com>
2025-06-13 12:40:01 +0200

    Update auth route to use app.use()

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,11 +5,12 @@ const AuthService = require("../services/AuthService");
 const AuthServiceInstance = new AuthService();
 
 module.exports = (app, passport) => {
+  app.use("/api/auth", router);
+
   // Registration Endpoint:
   router.post("/register", async (req, res, next) => {
     try {
       const data = req.body;
-
       const response = await AuthServiceInstance.register(data);
       res.status(200).send(response);
     } catch (err) {

364a9b65c1c3f0972003afcb00a86ef9c6279030
Marnus von Wielligh <52937047+marnusvw@users.noreply.github.com>
2025-06-24 20:15:36 +0200

    Update routes index.

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const passport = require("../loaders/passport");
 const userRouter = require("./user");
 const authRouter = require("./auth");
 const recipeRouter = require("./recipe");
-
+const test = require("./test");
 module.exports = (app, passport) => {
   authRouter(app, passport);
   userRouter(app);

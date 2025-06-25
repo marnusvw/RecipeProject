07fcb5b4bef1afedc24f31dda6126a57dd9318c8
Marnus von Wielligh <52937047+marnusvw@users.noreply.github.com>
2025-06-25 09:34:52 +0200

    Remove unused test code.

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,9 @@ const passport = require("../loaders/passport");
 const userRouter = require("./user");
 const authRouter = require("./auth");
 const recipeRouter = require("./recipe");
-const test = require("./test");
+
 module.exports = (app, passport) => {
   authRouter(app, passport);
   userRouter(app);
   recipeRouter(app);
-  test(app);
 };

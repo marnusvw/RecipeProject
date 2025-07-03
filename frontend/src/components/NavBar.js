679769a5aec194831977148c7d399d268723f292
Marnus von Wielligh <52937047+marnusvw@users.noreply.github.com>
2025-07-03 18:05:23 +0200

    Added a logo to the navbar.

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,6 +2,7 @@ import { NavLink, useNavigate } from "react-router-dom";
 import styles from "./styles/NavBar.module.css";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import Logo from "./resources/Logo.png";
 
 function NavBar({ loggedIn }) {
   const { logout } = useContext(AuthContext);
@@ -16,6 +17,9 @@ function NavBar({ loggedIn }) {
   return (
     <div>
       <header className={styles.navWrapper}>
+        <NavLink to="/">
+          <img className={styles.logo} src={Logo} alt="Chef hat logo"></img>
+        </NavLink>
         <ul className={styles.navLeft}>
           <li>
             <NavLink
